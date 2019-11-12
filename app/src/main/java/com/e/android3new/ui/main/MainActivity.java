76eddcd8fbc212715f8002ba8778a60df7a7c250
Malikovich <md.malikovich@gmail.com>
2019-11-12 23:16:14 +0600

    2

--- app/src/main/java/com/e/android3new/ui/main/MainActivity.java
+++ app/src/main/java/com/e/android3new/ui/main/MainActivity.java
@@ -2,9 +2,12 @@ package com.e.android3new.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.e.android3new.R;
+import com.e.android3new.ui.splashscreen.SplashActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
     }
 }
